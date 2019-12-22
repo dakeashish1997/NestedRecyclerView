2c802a80fe65182a53b6180d6a93e39e5d4b60ad
Ashish Dake <dakeashish1997@gmail.com>
2019-12-22 21:58:35 +0530

    Add files via upload
    Run this file via server

---
+++ datalist.php
@@ -0,0 +1,186 @@
+[
+	{
+		"classname": "7",
+		"students":
+		[
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			}
+		]
+	},
+	{
+		"classname": "8",
+		"students":
+		[
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			}
+		]
+	},
+	{
+		"classname": "9",
+		"students":
+		[
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			}
+		]
+	},
+	{
+		"classname": "10",
+		"students":
+		[
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			}
+		]
+	},
+	{
+		"classname": "11",
+		"students":
+		[
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			}
+		]
+	},
+	{
+		"classname": "12",
+		"students":
+		[
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			},
+			{
+				"studentname": "name",
+				"studentphoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBpgPrN-cSrAVl4cCB5GY91pJsFCdeKR9JSydXZnWxrmRnM5qB"
+			}
+		]
+	}
+]
